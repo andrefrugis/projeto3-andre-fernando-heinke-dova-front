4f6e139f92b0140a8bb0da4af0980d20b14e37ca
vitorpadovani <vipadovani28@gmail.com>
2024-05-21 08:28:56 -0300

    login atualizado

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -66,23 +66,23 @@ function Login({ onLoginSuccess }) {
             type="text"
             value={username}
             className="form-control"
-            aria-describedby="userHelp"
             onChange={(e) => setUsername(e.target.value)}
           />
-          <div id="userHelp" class="form-text">We'll never share your information with anyone else.</div>
         </div>
-        <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input 
-            type="email" 
-            className="form-control" 
-            id="exampleInputEmail1" 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
+    {isSigningUp && (
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <input 
+        type="email" 
+        className="form-control" 
+        id="exampleInputEmail1" 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+      </div>
+    )}
         <div className="mb-3">
           <label for="exampleInputPassword1" className="form-label">Password</label>
           <input
